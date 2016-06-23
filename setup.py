5061e7fff9d45cf653de0e1cfc62374f1d8a8d78
rushing3 <arushing95@gmail.com>
2016-06-23 14:26:18 -0400

    Initial commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='FlaskApp',
+setup(name='YourAppName',
       version='1.0',
-      description='A basic Flask app with static files',
-      author='Ryan Jarvinen',
-      author_email='ryanj@redhat.com',
+      description='OpenShift App',
+      author='Your Name',
+      author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+      install_requires=['Flask>=0.10.1', 'Flask-Bootstrap>=3.3.5.2']
      )
