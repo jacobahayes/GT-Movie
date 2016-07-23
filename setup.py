47f1a94ea89858f8a58c8c76d6e6e40a18553a39
rushing3 <arushing95@gmail.com>
2016-07-23 13:49:20 -0400

    Added installs

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='GTMovie',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.10.1', 'Flask-Bootstrap>=3.3.5.2']
+      install_requires=['flask_mysqldb','Flask>=0.10.1', 'Flask-Bootstrap>=3.3.5.2']
      )
