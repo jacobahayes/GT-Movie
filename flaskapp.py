3d252320d6ad6c7273705ff1293255ef437fae08
rushing3 <arushing95@gmail.com>
2016-07-23 17:15:03 -0400

    fix thing

--- flaskapp.py
+++ flaskapp.py
@@ -2,26 +2,26 @@ import os
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
-#from flask_mysqldb import MySQL
-#
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
-#app.config.from_pyfile('flaskapp.cfg')
-#app.config['MYSQL_USER'] = 'admingu2v3JA'
-#app.config['MYSQL_PASSWORD'] = '4eaeGBP2ZlDh'
-#app.config['MYSQL_DB'] = 'gtmovie'
-#app.config['MYSQL_HOST'] = '127.6.155.2'
-#app.config['MYSQL_PORT'] = 3306
-#mysql = MySQL()
-#mysql.init_app(app)
+app.config.from_pyfile('flaskapp.cfg')
+app.config['MYSQL_USER'] = 'admingu2v3JA'
+app.config['MYSQL_PASSWORD'] = '4eaeGBP2ZlDh'
+app.config['MYSQL_DB'] = 'gtmovie'
+app.config['MYSQL_HOST'] = '127.6.155.2'
+app.config['MYSQL_PORT'] = 3306
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/')
 def index():
-    #try:
-    #    #conn = mysql.connect()
-    #    cursor = mysql.connection.cursor()
-    #    return("okay")
-    #except Exception as e:
-    #    return(str(e))
+    try:
+        #conn = mysql.connect()
+        cursor = mysql.connection.cursor()
+        return("okay")
+    except Exception as e:
+        return(str(e))
 
 
     #Query for login
