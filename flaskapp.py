e8bfea28a52937dadf2ecd933687b9f6249f6d4d
rushing3 <arushing95@gmail.com>
2016-07-25 15:23:11 -0400

    Fix ip and port

--- flaskapp.py
+++ flaskapp.py
@@ -9,10 +9,10 @@ app.config.from_pyfile('flaskapp.cfg')
 app.config['MYSQL_USER'] = 'admingu2v3JA'
 app.config['MYSQL_PASSWORD'] = '4eaeGBP2ZlDh'
 app.config['MYSQL_DB'] = 'gtmovie'
-#app.config['MYSQL_HOST'] = '127.6.155.2'
-#app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_HOST'] = '127.6.155.2'
+app.config['MYSQL_PORT'] = 3306
+#app.config['MYSQL_HOST'] = '127.0.0.1'
+#app.config['MYSQL_PORT'] = 3307
 mysql = MySQL()
 mysql.init_app(app)
 
